8882bfa4ca7c3bcdaf09df799fe3292b6406db03
rc100808 <rishav.chakraborty@oracle.com>
2024-10-26 00:36:11 +0530

    colors fix

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd("colorscheme rose-pine-moon")
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })         -- Set background of main editor to transparent
+		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })     -- Set background of floating windows to transparent
 	end,
 }
